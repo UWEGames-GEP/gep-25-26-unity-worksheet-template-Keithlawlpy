4b69727336688a21c9d809bb071560e202e06643
Ethan <ethanlau88@gmail.com>
2025-10-06 12:22:29 +0100

    added Game Manager and State Machine

---
+++ Assets/TutorialInfo/Scripts/Game Manager.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        GAMEPLAY, PAUSE
+    }
+    public GameState state;
+    private bool hasChangedState;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        state = GameState.GAMEPLAY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state  == GameState.GAMEPLAY)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("Pressed esc one");
+                state = GameState.PAUSE;
+                hasChangedState = true;
+            }
+        }
+        else if (state == GameState.PAUSE)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("Pressed esc two");
+                state = GameState.GAMEPLAY;
+                hasChangedState = true;
+            
+            }
+
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if(hasChangedState)
+        {
+            hasChangedState = false;
+
+            if(state == GameState.GAMEPLAY)
+            {
+                Time.timeScale = 1f;
+            }
+            else if(state == GameState.PAUSE)
+            {
+                Time.timeScale = 0f;
+            }
+
+        }
+    }
+}
